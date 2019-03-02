8dffbe0d64150ca61db43e3912eccd6b17322c9f
CrisTovilla <163201@ids.upchiapas.edu.mx>
2019-03-01 19:57:37 -0600

    first commit

---
+++ coach.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+
+var net = require('net');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+  for (var k2 in interfaces[k]) {
+    var address = interfaces[k][k2];
+    if (address.family === 'IPv4' && !address.internal) {
+      addresses.push(address.address);
+    }
+  }
+}
+
+
+var HOST = '127.0.0.1';
+var PORT = 5000;
+server.listen(5678);
+var web_sockets = [];
+
+
+
+
+io.on('connection', function(socket) {
+    web_sockets.push(socket)
+
+    socket.on('disconnect', function() {
+          var idx = web_sockets.indexOf(socket);
+          if (idx != -1) {
+            web_sockets.splice(idx, 1);
+          }
+    });
+
+    socket.on('end', function() {
+        
+    });
+
+    socket.on('error', function() {
+
+    });
+
+    socket.on('timeout', function() {
+        
+    });
+
+    socket.on('close', function() {
+        
+    });
+
+});
+
+io.on('error',function(err){ 
+  console.error(err)
+});
+
+
+
+net.createServer(function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);    
+    sock.on('data', function(data) {
+    });
+
+}).listen(PORT, HOST);
+
