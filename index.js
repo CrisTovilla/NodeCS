d1b2bd9b6fa3f97efe00ae62484f4280bb6930a0
CrisTovilla <163201@ids.upchiapas.edu.mx>
2019-07-24 19:39:05 -0500

    Database SQLITE

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
+
+const db = require('sqlite')
 var express = require('express');
 var app = express();
+app.use(express.json());
+
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/category/:id', async (req, res, next) => {
+  try {
+    await db.open('./database.sqlite', { Promise })
+    const category = await  db.get('SELECT * FROM Category WHERE id = ?', req.params.id)
+    
+    res.status(200).json({category})
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/category/', async (req, res, next) => {
+  try {
+    await db.open('./database.sqlite', { Promise })
+    const categories = await  db.all('SELECT * FROM Category')
+    
+    res.status(200).json({categories})
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/category/',async function (req, res,next) {
+  try {
+    await db.open('./database.sqlite', { Promise })
+    const category = await db.run('INSERT INTO Category(name) VALUES(?)', [req.body.name])
+    
+    res.status(200).json({category })
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/category/:id',async function (req, res,next) {
+  try {
+    await db.open('./database.sqlite', { Promise })
+    const category = await db.run('UPDATE Category set name= ? WHERE id = ?', [req.body.name,req.params.id])
+    
+    res.status(200).json({category })
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
+
 app.listen(3000, function () {
   console.log('app listening on port 3000!');
 });
+
+
 
 module.exports = app;
 
--- test/test.js
+++ test/test.js
@@ -1,20 +1,95 @@
 var request = require('supertest');
 var app = require('../index.js');
+const db = require('sqlite')
 
-
-describe('Route GET "/"', function() {
- it('should respond with Hello World!', function(done) {
-    //navigate to root and check the the response is "Hello World!"
-    request(app).get('/').expect('Hello World!', function(err){
-        if (err){
-            done(err);
-            console.log("Error in Test")
-        }
-        else {
-            console.log("Succes Test");
-            done();
-            process.exit(0)
-        }
+describe('Api Routes', function() {
+    before(async function(){
+        await db.open('./database.sqlite', { Promise })
+        await db.migrate({ force: 'last' })
     });
- });
+    after(function(done) {
+        // runs after all tests in this block
+        process.exit(0)
+    });
+    
+    describe('Route GET "/"', function() {
+        it('should respond with Hello World!', function(done) {
+           //navigate to root and check the the response is "Hello World!"
+           request(app).get('/').expect('Hello World!', function(err){
+               if (err){
+                   done(err);
+               }
+               else {
+                   done();
+               }
+           });
+        });
+       });
+    
+    describe('Route GET "/category/:id"', function() {
+        it('should respond with {"category":{"id":1,"name":"Test"}}', function(done) {
+           request(app).get('/category/1')
+           .expect(200, {
+               category :{
+                   id:1,
+                   name:"Test"
+               }
+            }, done)
+        });
+    });
+
+    describe('Route POST /category/', function() {
+        it('responds with json', function(done) {
+          request(app).post('/category/')
+            .send({name: 'Games'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) return done(err);
+              done();
+            });
+        });
+      });
+
+      
+    describe('Route PUT /category/:id', function() {
+        it('responds with json', function(done) {
+          request(app).put('/category/1')
+            .send({name: 'Role-Playing'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) return done(err);
+              done();
+            });
+        });
+      });
+
+    describe('Route GET "/category/"', function() {
+        it('should respond with all the categories {"categories":[{"id":1,"name":"Role-Playing"}},{"category":{"id":2,"name":"Games"}]}', function(done) {
+           request(app).get('/category/')
+           .expect(200, {
+               categories :
+               [
+                    {
+                        id:1,
+                        name:"Role-Playing"
+                    },
+                    {
+                        id:2,
+                        name:"Games"
+                    }
+                ]
+            }, done)
+        });
+    });
+
+    
+ 
 });
+
+
+
+
